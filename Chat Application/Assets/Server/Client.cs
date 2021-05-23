b3a765ca6f619b82cf7c292b56d3f6415508948c
Mert <mertiq@icloud.com>
2021-05-23 14:56:57 +0300

    Client

---
+++ Chat Application/Assets/Server/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Client : MonoBehaviour
+{
+    [Header("Server Things")]
+    bool socketReady;
+    TcpClient socket;
+    NetworkStream networkStream;
+    BinaryWriter binaryWriter;
+    BinaryReader binaryReader;
+    string host = "127.0.0.1";
+    int port = 5000;
+
+    [Header("Client Things")]
+    List<GameObject> messages = new List<GameObject>();
+    string name;
+    int id;
+
+    [Header("enterance panel")]
+    [SerializeField] InputField nameInputField;
+
+    private void Update()
+    {
+        if (socketReady)
+        {
+            if (networkStream.DataAvailable)
+            {
+               
+            }
+        }
+    }
+
+
+    public void ConnectToServer()
+    {
+        if (socketReady)
+            return;
+
+        if (!nameInputField.text.Equals(""))
+        {
+            socket = new TcpClient("127.0.0.1", 5000);
+            networkStream = socket.GetStream();
+            binaryWriter = new BinaryWriter(networkStream);
+            binaryReader = new BinaryReader(networkStream);
+            socketReady = true;
+
+        }
+
+    }
+
+}

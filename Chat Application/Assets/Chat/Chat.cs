b281cb0dbf22b14ac13f5b5385368798c0bbbf7f
Mert <mertiq@icloud.com>
2021-05-23 15:51:39 +0300

    Chat and Message

---
+++ Chat Application/Assets/Chat/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class Chat
+{
+    public List<ServerClient> clients;
+    public List<Message> messages;
+
+    public Chat(ServerClient client1, ServerClient client2)
+    {
+        messages = new List<Message>();
+        clients = new List<ServerClient>();
+        clients.Add(client1);
+        clients.Add(client2);
+    }
+}
---
+++ Chat Application/Assets/Chat/ChatRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatRoom
+{
+    public List<Message> messages;
+    public List<Client> clients;
+
+    public ChatRoom(List<Client> clients)
+    {
+        messages = new List<Message>();
+        this.clients = clients;
+    }
+}
